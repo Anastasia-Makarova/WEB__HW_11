89c504b7393a5594a674a4a024c0fb7e4f53c8f7
Anastasia-Makarova <140186425+Anastasia-Makarova@users.noreply.github.com>
2024-02-18 10:35:24 +0100

    db connected

--- src/database/db.py
+++ src/database/db.py
@@ -7,30 +7,29 @@ from src.config.config import config
 
 
 
-class DatabaseSesssionManager:
-    def __init__(self, url: str) -> None:
+class DatabaseSessionManager:
+    def __init__(self, url: str):
         self._engine: AsyncEngine | None = create_async_engine(url)
-        self._session_maker = async_sessionmaker(autoflush=False, 
-                                                      autocommit=False, 
-                                                      bind=self._engine)
-        
+        self._session_maker: async_sessionmaker = async_sessionmaker(autoflush=False, autocommit=False,
+                                                                     bind=self._engine)
 
-@contextlib.asynccontextmanager
-async def session(self):
-    if self._session_maker is None:
-        raise Exception('Sesion is not initialized')
-    session = self._session_maker()
-    try:
-        yield session
-    except Exception as err:
-        print(err)
-        await session.rollback()
-    finally:
-        await session.close()
+    @contextlib.asynccontextmanager
+    async def session(self):
+        if self._session_maker is None:
+            raise Exception("Session is not initialized")
+        session = self._session_maker()
+        try:
+            yield session
+        except Exception as err:
+            print(err)
+            await session.rollback()
+        finally:
+            await session.close()
 
 
-sessionmanager = DatabaseSesssionManager(config.DB_URL)
+sessionmanager = DatabaseSessionManager(config.DB_URL)
+
 
 async def get_db():
-    with sessionmanager.session() as session:
+    async with sessionmanager.session() as session:
         yield session
