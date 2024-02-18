b5847e19550ab71137138239b2f609a6e7ab4db1
Anastasia-Makarova <140186425+Anastasia-Makarova@users.noreply.github.com>
2024-02-18 09:34:06 +0100

    alembic

---
+++ src/database/db.py
@@ -0,0 +1,39 @@
+import contextlib
+
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncConnection, async_sessionmaker,create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
+from src.config.config import config
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class DatabaseSesssionManager:
+    def __init__(self, url: str) -> None:
+        self._engine: AsyncEngine | None = create_async_engine(url)
+        self._session_maker = async_sessionmaker(autoflush=False, 
+                                                      autocommit=False, 
+                                                      bind=self._engine)
+        
+
+@contextlib.asynccontextmanager
+async def session(self):
+    if self._session_maker is None:
+        raise Exception('Sesion is not initialized')
+    session = self._session_maker()
+    try:
+        yield session
+    except Exception as err:
+        print(err)
+        await session.rollback()
+    finally:
+        await session.close()
+
+
+sessionmanager = DatabaseSesssionManager(config.DB_URL)
+
+async def get_db():
+    with sessionmanager.session() as session:
+        yield session
---
+++ src/entity/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String, Date
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database.db import Base
+
+
+class Contact(Base):
+    __tablename__ = "contacts"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+    surname: Mapped[str] = mapped_column(String(50))
+    phone_number: Mapped[str] = mapped_column(String(15))
+    email: Mapped[str] = mapped_column(String(40))
+    birthday: Mapped[str] = mapped_column(Date)
+    notes: Mapped[str] = mapped_column(String(200))
